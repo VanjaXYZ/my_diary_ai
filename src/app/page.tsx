501f9f80bc2d42e1fd2c431d158394c37c03e5ae
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-04-14 23:28:30 +0100

    set content into separate components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
-import Image from "next/image";
-import AiRobot from "./assets/2151008306.jpg";
+import IntroRobotImage from "./components/(homepage)/IntroRobotImage";
+import IntroText from "./components/(homepage)/IntroText";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col md:flex-row items-center justify-between p-24 container border m-auto">
-      <section className="space-y-8">
-        <h1 className="text-5xl text-balance">
-          MyDiaryAI - Your Personal Journal Companion
-        </h1>
-        <p className="text-xl text-balance">
-          MyDiaryAI helps users organize their thoughts, track their emotions,
-          and gain insights through AI-powered analysis
-        </p>
-      </section>
-      <section className="relative">
-        <Image src={AiRobot} alt="ai_robot" width={650} height={650} />
-      </section>
+    <main className="flex min-h-screen flex-col lg:flex-row items-center justify-between p-24 container border m-auto space-y-12">
+      <IntroText />
+      <IntroRobotImage />
     </main>
   );
 }
