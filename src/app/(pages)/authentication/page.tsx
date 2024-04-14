d05b7d51988dcbe68bf3d10b01e6fe3814945a25
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-04-15 00:26:21 +0100

    comp.: separated login and registration forms to separate components

--- src/app/(pages)/authentication/page.tsx
+++ src/app/(pages)/authentication/page.tsx
@@ -1,73 +1,16 @@
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import LoginForm from "./components/LoginForm";
+import RegistrationForm from "./components/RegistrationForm";
 
 const Authentication = () => {
   return (
-    <Tabs defaultValue="account" className="max-w-[400px] m-auto mt-20">
+    <Tabs defaultValue="login" className="max-w-[400px] m-auto mt-20">
       <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value="account">Login</TabsTrigger>
-        <TabsTrigger value="password">Register</TabsTrigger>
+        <TabsTrigger value="login">Login</TabsTrigger>
+        <TabsTrigger value="register">Register</TabsTrigger>
       </TabsList>
-      <TabsContent value="account">
-        <Card>
-          <CardHeader>
-            <CardTitle>Login</CardTitle>
-            <CardDescription>Login to your account.</CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="name">Username</Label>
-              <Input id="name" defaultValue="Daydreamer" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="username">Password</Label>
-              <Input id="username" defaultValue="********" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Login</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-      <TabsContent value="password">
-        <Card>
-          <CardHeader>
-            <CardTitle>Register</CardTitle>
-            <CardDescription>Register your new account here.</CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="email">Email</Label>
-              <Input id="email" type="email" required />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="newUsername">Username</Label>
-              <Input id="newUsername" type="text" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="newPassword">Password</Label>
-              <Input id="newPassword" type="password" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="repeatNewPassword">Confirm Password</Label>
-              <Input id="repeatNewPassword" type="password" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Register</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
+      <LoginForm />
+      <RegistrationForm />
     </Tabs>
   );
 };
