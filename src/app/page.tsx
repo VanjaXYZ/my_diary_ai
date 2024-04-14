98c47f6a358202b27f7697c41d34b1109b6511ef
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-04-14 23:51:45 +0100

    style: add bg-white

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import IntroText from "./components/(homepage)/IntroText";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col lg:flex-row items-center justify-between p-24 container border m-auto space-y-12">
+    <main className="flex min-h-screen flex-col lg:flex-row items-center justify-between p-24 container bg-white m-auto space-y-12">
       <IntroText />
       <IntroRobotImage />
     </main>
