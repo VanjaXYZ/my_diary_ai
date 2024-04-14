ef43bfeae7be837cbb742ad79479348d16459e5e
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-04-14 23:28:33 +0100

    created

---
+++ src/app/components/(homepage)/IntroRobotImage.tsx
@@ -0,0 +1,21 @@
+import AiRobot from "@/app/assets/ai_image.jpg";
+import Image from "next/image";
+import ChatExample from "../(wrappers)/ChatExample";
+const IntroRobotImage = () => {
+  return (
+    <section className="relative hidden sm:block">
+      <section className="absolute -left-24 top-12">
+        <ChatExample />
+      </section>
+      <Image
+        src={AiRobot}
+        alt="ai_robot"
+        width={650}
+        height={650}
+        className="rounded-lg"
+      />
+    </section>
+  );
+};
+
+export default IntroRobotImage;
---
+++ src/app/components/(homepage)/IntroText.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const IntroText = () => {
+  return (
+    <section className="space-y-8 text-center">
+      <h1 className="text-5xl text-balance">
+        MyDiaryAI - Your Personal Journal Companion
+      </h1>
+      <p className="text-xl text-balance">
+        MyDiaryAI helps users organize their thoughts, track their emotions, and
+        gain insights through AI-powered analysis
+      </p>
+    </section>
+  );
+};
+
+export default IntroText;
