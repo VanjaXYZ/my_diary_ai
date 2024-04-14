742da9d0a9ce8d7cfeb8ef4ea46f89e5ab847b4a
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-04-14 23:50:37 +0100

    created

---
+++ src/app/components/(homepage)/ChatExample.tsx
@@ -0,0 +1,15 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+const ChatExample = () => {
+  return (
+    <div className="flex items-center px-2 rounded-lg bg-primary border border-slate-500 py-1 text-white space-x-3">
+      <Avatar>
+        <AvatarImage src="robot_avatar.jpg" alt="robot_avatar_img" />
+        <AvatarFallback>AI</AvatarFallback>
+      </Avatar>
+      <p>Hello! How can I assist you?</p>
+    </div>
+  );
+};
+
+export default ChatExample;
