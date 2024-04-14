4f2668d06fbcbf61acfe0a67d33199f42ef0039c
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-04-14 22:38:07 +0100

    add ai robot image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import Image from "next/image";
+import AiRobot from "./assets/2151008306.jpg";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 container border m-auto">
+    <main className="flex min-h-screen flex-col md:flex-row items-center justify-between p-24 container border m-auto">
       <section className="space-y-8">
         <h1 className="text-5xl text-balance">
           MyDiaryAI - Your Personal Journal Companion
@@ -9,6 +11,9 @@ export default function Home() {
           MyDiaryAI helps users organize their thoughts, track their emotions,
           and gain insights through AI-powered analysis
         </p>
+      </section>
+      <section className="relative">
+        <Image src={AiRobot} alt="ai_robot" width={650} height={650} />
       </section>
     </main>
   );
