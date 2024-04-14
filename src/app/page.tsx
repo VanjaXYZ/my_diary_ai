0c340d090ce0254d15490445c5cd451f28238129
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-04-14 22:28:51 +0100

    add header and intro text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 container border m-auto">
+      <section className="space-y-8">
+        <h1 className="text-5xl text-balance">
+          MyDiaryAI - Your Personal Journal Companion
+        </h1>
+        <p className="text-xl text-balance">
+          MyDiaryAI helps users organize their thoughts, track their emotions,
+          and gain insights through AI-powered analysis
+        </p>
+      </section>
+    </main>
   );
 }
