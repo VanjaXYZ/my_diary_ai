42bf082e65450346287ae4b6cefbd73a637ac53b
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-04-18 05:59:04 +0100

    style: removed classname cause layout already styling this way

--- src/app/(pages)/diary/page.tsx
+++ src/app/(pages)/diary/page.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Diary = () => {
-  return (
-    <div className="flex min-h-screen flex-col lg:flex-row justify-between p-8 bg-white m-auto space-y-12">
-      Hello from Diary page.
-    </div>
-  );
+  return <div>Hello from Diary page.</div>;
 };
 
 export default Diary;
