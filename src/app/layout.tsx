22b6b6227078275345b9919707cacb25d0b7026a
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-04-18 05:37:26 +0100

    add container class

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className + " bg-slate-100/80"}>{children}</body>
+      <body className={inter.className + " bg-slate-100/80 container"}>
+        {children}
+      </body>
     </html>
   );
 }
