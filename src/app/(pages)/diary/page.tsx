b2efd5d495f30c310fb2da03b7487df1425e6dbb
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-04-18 05:37:59 +0100

    created

---
+++ src/app/(pages)/diary/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Diary = () => {
+  return (
+    <div className="flex min-h-screen flex-col lg:flex-row justify-between p-8 bg-white m-auto space-y-12">
+      Hello from Diary page.
+    </div>
+  );
+};
+
+export default Diary;
