3e8aa4631c8b96fdec11385fd112fbe33b167bdf
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-05-08 21:09:02 +0100

    created form wrapper

---
+++ src/app/(pages)/authentication/components/FormWrapper.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { TabsContent } from "@/components/ui/tabs";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+const FormWrapper = ({ value, title, description, children }) => {
+  return (
+    <TabsContent value={value}>
+      <Card>
+        <CardHeader>
+          <CardTitle>{title}</CardTitle>
+          <CardDescription>{description}</CardDescription>
+        </CardHeader>
+        {children}
+      </Card>
+    </TabsContent>
+  );
+};
+
+export default FormWrapper;
