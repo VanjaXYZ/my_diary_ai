d0964ec55346c55ddeffe0b3921f2b88e70f1d36
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-05-08 21:41:52 +0100

    created register validation schema

--- src/app/schemas/validations.ts
+++ src/app/schemas/validations.ts
@@ -2,9 +2,24 @@ import { z } from "zod";
 
 export const loginSchema = z.object({
   username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
+    message: "Username must be at least 4 characters.",
   }),
   password: z.string().min(8, {
     message: "Password must be at least 8 characters.",
   }),
 });
+export const registrationSchema = z
+  .object({
+    email: z.string().email({ message: "Invalid email." }),
+    username: z.string().min(2, {
+      message: "Username must be at least 4 characters.",
+    }),
+    password: z.string().min(8, {
+      message: "Password must be at least 8 characters.",
+    }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match.",
+    path: ["confirmPassword"],
+  });
