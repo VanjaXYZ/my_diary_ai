6941bde61610bebdbe6f8f7e2bd5c8acf0798c31
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-05-08 21:41:21 +0100

    refactored

--- src/app/(pages)/authentication/components/RegistrationForm.tsx
+++ src/app/(pages)/authentication/components/RegistrationForm.tsx
@@ -9,43 +9,52 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { Form } from "@/components/ui/form";
 import { Label } from "@/components/ui/label";
 import { TabsContent } from "@/components/ui/tabs";
+import FormWrapper from "./FormWrapper";
 import React from "react";
+import { RegistrationFormValidation } from "@/app/types/types";
+import { registerTypeZod } from "../../../types/types";
+import { registrationSchema } from "@/app/schemas/validations";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import RegistrationInputs from "./RegistrationInputs";
 
-const RegistrationForm = () => {
+const RegistrationForm: React.FC<RegistrationFormValidation> = () => {
+  const form = useForm<registerTypeZod>({
+    resolver: zodResolver(registrationSchema),
+    defaultValues: {
+      email: "",
+      username: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  };
   return (
-    <TabsContent value="register">
-      <Card>
-        <CardHeader>
-          <CardTitle>Register</CardTitle>
-          <CardDescription>Register your new account here.</CardDescription>
-        </CardHeader>
-        <CardContent className="space-y-2">
-          <div className="space-y-1">
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" type="email" required />
-          </div>
-          <div className="space-y-1">
-            <Label htmlFor="newUsername">Username</Label>
-            <Input id="newUsername" type="text" />
-          </div>
-          <div className="space-y-1">
-            <Label htmlFor="newPassword">Password</Label>
-            <Input id="newPassword" type="password" />
-          </div>
-          <div className="space-y-1">
-            <Label htmlFor="repeatNewPassword">Confirm Password</Label>
-            <Input id="repeatNewPassword" type="password" />
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button onClick={() => console.log("Registration complete!")}>
-            Register
-          </Button>
-        </CardFooter>
-      </Card>
-    </TabsContent>
+    <FormWrapper
+      value={"register"}
+      title={"Register"}
+      description={"Register your new account here."}
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <CardContent className="space-y-2">
+            <RegistrationInputs form={form} />
+          </CardContent>
+          <CardFooter>
+            <Button type="submit">Register</Button>
+          </CardFooter>
+        </form>
+      </Form>
+    </FormWrapper>
   );
 };
 
