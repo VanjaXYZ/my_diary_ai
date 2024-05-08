63d94bdbff231cad235a73d6a09484b60e34f24e
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-05-08 21:41:42 +0100

    created register mock api

--- src/app/routes/_auth/route.ts
+++ src/app/routes/_auth/route.ts
@@ -11,3 +11,17 @@ export const loginUser = async (formData: FormData) => {
     console.error(error);
   }
 };
+
+export const registerUser = async (formData: FormData) => {
+  try {
+    const email = formData.get("email");
+    const username = formData.get("username");
+    const password = formData.get("password");
+    const confirmPassword = formData.get("confirmPassword");
+    if (username !== "" && password !== "") {
+      console.log("Login success!", username, password);
+    } else console.log("Login failed!");
+  } catch (error) {
+    console.error(error);
+  }
+};
