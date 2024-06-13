6e1bdecee95997861fa0e194a992a9f2eccb0817
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-13 17:58:25 +0100

    changed component name

--- src/app/(pages)/diary/layout.tsx
+++ src/app/(pages)/diary/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const layout = ({ children }: { children: React.ReactNode }) => {
+const diaryPageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen flex-col lg:flex-row justify-between p-8 bg-white m-auto space-y-12">
       {children}
@@ -8,4 +8,4 @@ const layout = ({ children }: { children: React.ReactNode }) => {
   );
 };
 
-export default layout;
+export default diaryPageLayout;
