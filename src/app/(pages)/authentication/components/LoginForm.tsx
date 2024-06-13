a1614de92f6132d7d08cc0c29ceefc1116758909
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-13 22:37:55 +0100

    set cookie token

--- src/app/(pages)/authentication/components/LoginForm.tsx
+++ src/app/(pages)/authentication/components/LoginForm.tsx
@@ -1,16 +1,19 @@
 "use client";
+import { createTokenCoookie } from "@/app/actions";
 import { loginSchema } from "@/app/schemas/validations";
-import { LoginFormValidation } from "@/app/types/types";
 import { Button } from "@/components/ui/button";
 import { CardContent, CardFooter } from "@/components/ui/card";
 import { Form } from "@/components/ui/form";
+import { toast } from "@/components/ui/use-toast";
 import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { Config } from "../../../../../config";
 import { loginTypeZod } from "../../../types/types";
 import FormWrapper from "./FormWrapper";
 import LoginInputs from "./LoginInputs";
-import { Config } from "../../../../../config";
 
 const LoginForm: React.FC = () => {
   const form = useForm<typeof loginTypeZod>({
@@ -20,15 +23,26 @@ const LoginForm: React.FC = () => {
       password: "",
     },
   });
+  const router = useRouter();
   const onSubmit = async (values: z.infer<typeof loginSchema>) => {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    // try {
-    // const response = await fetch(`${Config.baseURL}/user`)
-    // } catch (error) {
-    //   console.error();
-    // }
-    // console.log(values);
+    try {
+      const response = await axios.post(`${Config.baseURL}/login`, {
+        username: values.username,
+        password: values.password,
+      });
+      if (response.status === 201) {
+        createTokenCoookie(response?.data);
+        toast({
+          description: response?.data?.message,
+        });
+        router.replace("/diary");
+        return response;
+      }
+    } catch (error: any) {
+      toast({
+        description: error.response.data.message,
+      });
+    }
   };
   return (
     <FormWrapper
